a8c627f5947416e180acb0350bd1ff5c86883aae
CodeSandwich <igor.zuk@pm.me>
2019-07-31 16:32:12 +0200

    WIP

--- jormungandr-integration-tests/src/common/configuration/mod.rs
+++ jormungandr-integration-tests/src/common/configuration/mod.rs
@@ -61,6 +61,14 @@ fn get_working_directory() -> PathBuf {
     output_directory
 }
 
+pub fn get_openapi_path() -> PathBuf {
+    let mut path = get_working_directory();
+    path.pop();
+    path.pop();
+    path.push("doc/openapi.yaml");
+    path
+}
+
 lazy_static! {
     static ref NEXT_AVAILABLE_PORT_NUMBER: AtomicU16 = {
         let initial_port = rand::thread_rng().gen_range(6000, 60000);
